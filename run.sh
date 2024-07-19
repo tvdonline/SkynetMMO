e67e0dc4966f285dba46bceff58f313fdbe15c78
tvdonline <51505373+tvdonline@users.noreply.github.com>
2024-07-19 18:04:25 +0000

    Pending changes exported from your codespace

--- run.sh
+++ run.sh
@@ -4,9 +4,4 @@ export DAEMON=false
 if [ ! -d "log" ]; then
   mkdir log
 fi
-if [ $(ps e -u ${USER} | grep -v grep | grep $(pwd) | grep skynet | wc -l) != 0 ]
-then
-    echo "server is already running, please execute ./stop.sh"
-else
-$ROOT/skynet/skynet $ROOT/config > log/$(date "+%Y%m%d-%H%M").log &
-fi
+$ROOT/skynet/skynet $ROOT/config
